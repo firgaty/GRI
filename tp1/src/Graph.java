a31282d3f3078bc8b8d43f34a0fbc298560158f9
felix_desmaretz <felix.desmaretz@protonmail.com>
2021-01-15 14:00:25 +0100

    Add addEdges

--- tp1/src/Graph.java
+++ tp1/src/Graph.java
@@ -1,13 +1,15 @@
+import java.util.Arrays;
+
 class Graph implements IGraph {
 
-    private int[][] adjacences;
+    private int[][] adjacencyLists;
 
     /**
      * Create new graph of n vertices
      * @param n Number of vertices
      */
     public Graph(int n) {
-        this.adjacences = new int[n][];
+        this.adjacencyLists = new int[n][];
     }
 
     @Override
@@ -26,5 +28,28 @@ class Graph implements IGraph {
     public int[] adjacencyList(int u) {
         // TODO
         return null;
+    }
+
+    @Override
+    public void addEdges(int u, int[] edges) {
+        adjacencyLists[u] = edges;
+    }
+    
+    @Override
+    public void addEdge(int u, int v) {
+        if (adjacencyLists[u] == null) {
+            adjacencyLists[u] = new int[] { v };
+            return;
+        }
+        
+        int[] newList = new int[adjacencyLists[u].length + 1];
+
+        for (int i = 0; i < adjacencyLists[u].length; i++) {
+            newList[i] = adjacencyLists[u][i];
+        }
+
+        newList[adjacencyLists[u].length] = v;
+
+        adjacencyLists[u] = newList;
     }
 }
--- tp1/src/IGraph.java
+++ tp1/src/IGraph.java
@@ -4,4 +4,21 @@ interface IGraph {
     int verticesCount();
 
     int[] adjacencyList(int u);
+
+    /**
+     * Add list to adjacency lists
+     * @param u Source vertice
+     * @param edges Neighbours
+     */
+    void addEdges(int u, int[] edges);
+
+    /**
+     * Add edge v to adjacency list of u
+     * 
+     * Costly, prefer using addEdges
+     * 
+     * @param u Source
+     * @param v Target
+     */
+    void addEdge(int u, int v);
 }
