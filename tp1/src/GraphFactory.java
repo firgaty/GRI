f17a80483d9c5dde80fbf234d44c3b8bb83c460a
felix_desmaretz <felix.desmaretz@protonmail.com>
2021-01-16 09:18:40 +0100

    Rewrite GraphFactory class

--- tp1/src/GraphFactory.java
+++ tp1/src/GraphFactory.java
@@ -2,36 +2,35 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.lang.Math;
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class GraphFactory {
 
-    public static int[] split_string(String s) {
-        String[] split = s.replaceAll("(\\t|\\s)+", " ").split(" ");
-        return new int[] { Integer.parseInt(split[0]), Integer.parseInt(split[1]) };
-    }
+    final Pattern pattern = Pattern.compile("^(\\d+)\\s+(\\d+)$");
 
-    public static void fill_graph(Graph g, String line) {
-        int[] n = split_string(line);
-        g.addEdge(n[0], n[1]);
-    }
+    public IGraph generate(String file, int maxNode, boolean oriented) {
+        int[] from = new int[maxNode];
+        int[] to = new int[maxNode];
 
-    public static int get_max_node(String file) {
         BufferedReader objReader = null;
-        int max = 0;
-        Boolean check_comment = true;
+
+        // O(n)
         try {
             String strCurrentLine;
             objReader = new BufferedReader(new FileReader(file));
-            while ((strCurrentLine = objReader.readLine()) != null) {
-                if (check_comment) {
-                    if (strCurrentLine.charAt(0) != '#') {
-                        int[] tmp = split_string(strCurrentLine);
-                        max = Math.max(tmp[0], tmp[1]);
-                        check_comment = false;
-                    }
+            while ((strCurrentLine = objReader.readLine()) != null && strCurrentLine.charAt(0) == '#')
+                System.out.println(strCurrentLine);
+
+            for (int i = 0; i < maxNode && strCurrentLine != null; i++, strCurrentLine = objReader.readLine()) {
+                Matcher matcher = pattern.matcher(strCurrentLine);
+
+                if (matcher.find()) {
+                    from[i] = Integer.parseInt(matcher.group(1));
+                    to[i] = Integer.parseInt(matcher.group(2));
                 } else {
-                    int[] tmp = split_string(strCurrentLine);
-                    max = Math.max(max, Math.max(tmp[0], tmp[1]));
+                    break;
                 }
             }
         } catch (IOException e) {
@@ -44,41 +43,220 @@ public class GraphFactory {
                 ex.printStackTrace();
             }
         }
-        return max;
+
+        // O(n log n)
+        return graphFromArrays(from, to, oriented);
     }
 
-    public static Graph create(String file, int max_node) {
-        BufferedReader objReader = null;
-        Boolean check_comment = true;
-        Graph g = new Graph(max_node + 1);
-        try {
-            String strCurrentLine;
-            objReader = new BufferedReader(new FileReader(file));
-            while ((strCurrentLine = objReader.readLine()) != null) {
-                if (check_comment) {
-                    if (strCurrentLine.charAt(0) != '#') {
-                        fill_graph(g, strCurrentLine);
-                        check_comment = false;
-                    }
-                } else
-                    fill_graph(g, strCurrentLine);
+    private IGraph graphFromArrays(int[] from, int[] to, boolean oriented) {
+        Integer[] idx;
+        // Calculate nodes
+        int max = 0;
+
+        System.out.println("from length: " + from.length);
+
+        idx = new Integer[from.length];
+        for (int i = 0; i < from.length; i++) {
+            idx[i] = i;
+        }
+
+        // First Sort
+        {
+            System.out.println("First sort...");
+            // O(n log n)
+            sort(idx, from, to);
+
+            // O(n)
+            int[] newFrom = new int[from.length];
+            int[] newTo = new int[from.length];
+
+            for (int i = 0; i < to.length; i++) {
+                // reorder
+                newFrom[i] = from[idx[i]];
+                newTo[i] = to[idx[i]];
+
+                // set max
+                if (from[i] > max)
+                    max = from[i];
+                if (to[i] > max)
+                    max = to[i];
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (objReader != null)
-                    objReader.close();
-            } catch (IOException ex) {
-                ex.printStackTrace();
+
+            from = newFrom;
+            to = newTo;
+        }
+
+        // Ask to free memory
+        System.gc();
+
+        // Add reverse edges if not oriented
+        if (!oriented) {
+            // O(n log n)
+            idx = new Integer[from.length * 2];
+            int[] newFrom = new int[from.length * 2];
+            int[] newTo = new int[from.length * 2];
+
+            for (int i = 0; i < from.length; i++) {
+                newFrom[i] = from[i];
+                newTo[i] = to[i];
+
+                // O(log n)
+                if (!contains(from, to, to[i], from[i], from.length - 1)
+                        && !contains(newFrom, newTo, to[i], from[i], newFrom.length - 1)) {
+                    newFrom[i + from.length] = to[i];
+                    newTo[i + from.length] = from[i];
+                }
+
+                idx[i] = i;
+                idx[i + from.length] = i + from.length;
+            }
+
+            to = newTo;
+            from = newFrom;
+        }
+
+        // Ask to free memory
+        System.gc();
+
+        // Sort
+        {
+            System.out.println("Second sort...");
+            // O(n log n)
+            sort(idx, from, to);
+
+            // O(n)
+            int[] newFrom = new int[from.length];
+            int[] newTo = new int[from.length];
+
+            for (int i = 0; i < to.length; i++) {
+                // reorder
+                newFrom[i] = from[idx[i]];
+                newTo[i] = to[idx[i]];
+            }
+
+            from = newFrom;
+            to = newTo;
+        }
+
+        // Ask to free memory
+        System.gc();
+
+        System.out.println("Create Graph");
+        // Create graph
+        IGraph g = new Graph(max + 1, oriented);
+
+        // O(n)
+        int inf = 0;
+        // Pass null values
+        for (; inf < to.length && from[inf] == 0 && to[inf] == 0; inf++)
+            ;
+
+        for (int i = inf; i < to.length; i++) {
+            // System.out.println(from[i] + ":" + to[i]);
+            if (from[i] != from[inf]) {
+                int[] edges = new int[i - inf];
+
+                for (int j = 0; j < i - inf; j++) {
+                    edges[j] = to[inf + j];
+                }
+
+                g.addEdges(from[inf], edges);
+
+                inf = i;
             }
         }
+
+        { // Last part
+            int[] edges = new int[to.length - inf];
+
+            for (int j = 0; j < to.length - inf; j++) {
+                edges[j] = to[inf + j];
+            }
+
+            g.addEdges(from[inf], edges);
+        }
+
+        System.out.println("Ok!");
         return g;
     }
 
-    /*
-     * public static void main(String[] args) { Graph g =
-     * GraphFactory.create(args[0], get_max_node(args[0])); }
-     */
+    private void sort(final Integer[] idx, final int[] from, final int[] to) {
+        // O(n log n)
+        // Sort, remove and sort again to filter out
+        Arrays.sort(idx, (a, b) -> {
+            if (from[a] < from[b])
+                return -1;
+            if (from[a] > from[b])
+                return 1;
+            if (to[a] < to[b])
+                return -1;
+            if (to[a] > to[b])
+                return 1;
+            return 0;
+        });
+    }
+
+    public boolean contains(final int[] from, final int[] to, final int keyA, final int keyB, int high) {
+        int low = 0;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int comp = compare(mid, keyA, keyB, from, to);
+
+            if (comp < 0) {
+                low = mid + 1;
+            } else if (comp > 0) {
+                high = mid - 1;
+            } else if (comp == 0) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public int compare(final int idx, final int fromVal, final int toVal, final int[] from, final int[] to) {
+        if (from[idx] < fromVal)
+            return -1;
+        if (from[idx] > fromVal)
+            return 1;
+        if (to[idx] < toVal)
+            return -1;
+        if (to[idx] > toVal)
+            return 1;
+        return 0;
+    }
+
+    private void filter(final int[] from, final int[] to) {
+        // O(n)
+        for (int i = 0; i < from.length;) {
+
+            for (int j = i + 1; j < from.length; j++) {
+                if (from[i] - from[j] == 0 && to[i] - to[j] == 0) {
+                    from[j] = 0;
+                    to[j] = 0;
+                } else {
+                    i += j - i;
+                    break;
+                }
+            }
+
+        }
+    }
+
+    public static void main(String[] args) {
+        String file = "web-BerkStan.txt";
+        // IGraph g = gf.generate(args[0], Integer.parseInt(args[1]),false);
+        GraphFactory gf = new GraphFactory();
+
+        // IGraph g = gf.generate(file, 28980, false);
+        IGraph g = gf.generate(file, 7600595, false);
+
+        // int[] from = new int[] { 1, 2, 5, 6, 7, 7, 7, 7, 8, 9, 10 };
+        // int[] to = new int[] { 5, 3, 6, 1, 2, 3, 4, 6, 1, 4, 10 };
+
+        // System.out.println(gf.compare(6, 6, 4, from, to));
+        // System.out.println(gf.contains(from, to, 6, 2, from.length -1));
+    }
 
 }
