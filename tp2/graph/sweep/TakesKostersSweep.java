7287c7ca8624a8f9d1959b7c584d3ec77fb93bea
felix_desmaretz <felix.desmaretz@protonmail.com>
2021-01-31 11:35:19 +0100

    Fix name of file & Finish exo 4

--- tp2/graph/sweep/TakesKostersSweep.java
+++ tp2/graph/sweep/TakesKostersSweep.java
@@ -1,5 +1,8 @@
 package graph.sweep;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+
 import graph.IGraph;
 
 /**
@@ -8,6 +11,89 @@ import graph.IGraph;
 public class TakesKostersSweep implements IGraphSweep {
     @Override
     public int sweep(IGraph g, int u) {
-        return 0; // TODO
+        int diamlow = 0;
+
+        boolean[] marked = new boolean[g.verticesCount()];
+        dfs(g, u, marked);
+
+        int[] eccsup = new int[g.verticesCount()];
+        Arrays.fill(eccsup, Integer.MAX_VALUE);
+
+        return _sweep(g, marked, eccsup, diamlow, u);
+    }
+
+    private int _sweep(IGraph g, boolean[] marked, int[] eccsup, int diamlow, int u) {
+        if (eccsup[u] <= diamlow) {
+            return diamlow;
+        }
+
+        int[] distance = new int[marked.length];
+
+        bfs(g, u, distance);
+
+        int[] ecc = max(distance);
+
+        if (ecc[1] > diamlow) {
+            diamlow = ecc[1];
+        }
+
+        for (int v = 0; v < marked.length; v++) {
+            if (marked[v]) {
+                int b = distance[v] + ecc[1];
+                if (b < eccsup[v]) {
+                    eccsup[v] = b;
+                }
+            }
+        }
+
+        return _sweep(g, marked, eccsup, diamlow, ecc[0]);
+    }
+
+    private void dfs(IGraph g, int u, boolean[] marked) {
+        marked[u] = true;
+
+        for (int v : g.adjacencyList(u)) {
+            if (!marked[v]) {
+                marked[v] = true;
+                dfs(g, v, marked);
+            }
+        }
+    }
+
+    private void bfs(IGraph g, int u, int[] distance) {
+        boolean[] marked = new boolean[distance.length];
+        LinkedList<Integer> queue = new LinkedList<Integer>();
+
+        queue.add(u);
+        marked[u] = true;
+
+        while (!queue.isEmpty()) {
+            int v = queue.removeFirst();
+
+            if (g.adjacencyList(u) == null) {
+                continue;
+            }
+
+            for (int w : g.adjacencyList(v)) {
+                if (!marked[w]) {
+                    marked[w] = true;
+                    distance[w] = distance[v] + 1;
+                    queue.addLast(w);
+                }
+            }
+        }
+    }
+
+    private int[] max(int[] array) {
+        int[] max = new int[2];
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max[1]) {
+                max[0] = i;
+                max[1] = array[i];
+            }
+        }
+
+        return max;
     }
 }
