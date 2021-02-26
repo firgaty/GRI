18b7890f4bacd515cc02eaca71e12fd587b67a57
felix_desmaretz <felix.desmaretz@protonmail.com>
2021-02-26 13:49:07 +0100

    Import files

--- tp4/TP4.java
+++ tp4/TP4.java
@@ -1,5 +1,8 @@
 import java.lang.Math;
 
+import graph.IGraph;
+import graph.parser.GraphParser;
+
 public class TP4 {
 
     public int[] graphe_aleatoire(int[] degrees) {
@@ -41,24 +44,25 @@ public class TP4 {
     }
 
     public static void main(String args[]) {
+        TP4 tp = new TP4();
         String type = args[0];
-        String fileName = args[1];
-        int nbNodes = Integer.parseInt(args[2]);
+        int nbNodes = Integer.parseInt(args[1]);
 
-        GraphParser gf = new GraphParser();
+        // GraphParser gf = new GraphParser();
         // on considere le graphe non oriente
-        IGraph g = gf.parse(fileName, nbNodes, false);
+        // IGraph g = gf.parse(fileName, nbNodes, false);
         int out = -1;
 
         switch (type) {
             case "racine": {
-                return 0;
+                tp.print_edges(tp.racine(nbNodes));
             }
             case "puissance": {
-                return 0;
+                double gamma = Double.parseDouble(args[2]);
+                tp.print_edges(tp.puissance(nbNodes, gamma));
             }
             case "exemple": {
-                return 0;
+                tp.print_edges(tp.exemple());
             }
             default: {
                 return;
