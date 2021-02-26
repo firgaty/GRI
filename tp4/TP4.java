ab9332966b1551b6cc1c89d2a7b561e9d5c0b945
felix_desmaretz <felix.desmaretz@protonmail.com>
2021-02-26 15:24:10 +0100

    Puissance

--- tp4/TP4.java
+++ tp4/TP4.java
@@ -1,40 +1,37 @@
 import java.lang.Math;
 import java.util.Random;
 
-import graph.IGraph;
-import graph.parser.GraphParser;
-
 public class TP4 {
 
     public void myShuffle(int[] t) {
-		Random rand = new Random();
-		for(int j = t.length - 1 ; j> 1 ; j --){
-			int r = rand.nextInt(j + 1);
-			int tmp = t[r];
-			t[r] = t[j];
-			t[j] = tmp;
-		}
-	}
+        Random rand = new Random();
+        for (int j = t.length - 1; j > 1; j--) {
+            int r = rand.nextInt(j + 1);
+            int tmp = t[r];
+            t[r] = t[j];
+            t[j] = tmp;
+        }
+    }
 
-    public int[] grapheAleatoire(int [] seq) {
-         //On calcule la taille de E
+    public int[] grapheAleatoire(int[] seq) {
+        // On calcule la taille de E
         int taille = 0;
-        for(int i = 0; i< seq.length; i++){
+        for (int i = 0; i < seq.length; i++) {
             taille += seq[i];
         }
 
         int[] E = new int[taille];
-        int pos  = 0;
-        for (int i =0 ; i< seq.length ; i ++){
+        int pos = 0;
+        for (int i = 0; i < seq.length; i++) {
             int tmp = 0;
-            while(tmp < seq[i]){
+            while (tmp < seq[i]) {
                 E[pos] = i;
-                tmp ++;
-                pos ++;
+                tmp++;
+                pos++;
             }
         }
         myShuffle(E);
-        return  E;
+        return E;
     }
 
     public int[] racine(int n) {
@@ -61,6 +58,46 @@ public class TP4 {
         return null;
     }
 
+    public int[] convertPuissance(int n, int[] list) {
+        int out[] = new int[n];
+        int counter = 0;
+
+        for (int i = 0; i < list.length; i++) {
+            for (int j = 0; j < list[i] && counter < n; j++) {
+                out[counter] = i;
+                counter++;
+            }
+        }
+
+        return out;
+    }
+
+    public int[] puissance2(int n, double gamma) {
+        double[] val = new double[n];
+        int[] out = new int[n];
+        double sum = 0;
+
+        for (int i = 1; i < n; i++) {
+            val[i] = Math.pow((double) i, -gamma);
+            sum += val[i];
+        }
+
+        double c = (double) 1 / sum;
+
+        int count = 0;
+        for (int i = 1; i < n; i++) {
+            out[i] = (int) Math.round(val[i] * c * n);
+            count += out[i] * i;
+        }
+
+        if (count % 2 == 1) {
+            out[1] += 1;
+            count += 1;
+        }
+
+        return convertPuissance(count, out);
+    }
+
     public void print_edges(int[] edges) {
         for (int i = 0; i < edges.length; i += 2) {
             System.out.println(Integer.toString(edges[i]) + " " + Integer.toString(edges[i + 1]));
@@ -70,12 +107,12 @@ public class TP4 {
     public static void main(String args[]) {
         TP4 tp = new TP4();
         String type = args[0];
-        
+
         // GraphParser gf = new GraphParser();
         // on considere le graphe non oriente
         // IGraph g = gf.parse(fileName, nbNodes, false);
         int out = -1;
-        
+
         switch (type) {
             case "racine": {
                 int nbNodes = Integer.parseInt(args[1]);
@@ -85,12 +122,12 @@ public class TP4 {
             case "puissance": {
                 int nbNodes = Integer.parseInt(args[1]);
                 double gamma = Double.parseDouble(args[2]);
-                tp.print_edges(tp.grapheAleatoire(tp.puissance(nbNodes, gamma)));
+                tp.print_edges(tp.grapheAleatoire(tp.puissance2(nbNodes, gamma)));
                 break;
             }
             case "exemple": {
-                int [] def = new int[] {1,2,1,4} ;
-                int [] tab = tp.grapheAleatoire(def);
+                int[] def = new int[] { 1, 2, 1, 4 };
+                int[] tab = tp.grapheAleatoire(def);
                 tp.print_edges(tab);
                 break;
             }
