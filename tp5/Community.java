fc733ee4718ee1d4e4549b32d6cfe70e7d31f2ec
felix_desmaretz <felix.desmaretz@protonmail.com>
2021-03-12 14:49:46 +0100

    Remove from

--- tp5/Community.java
+++ tp5/Community.java
@@ -1,46 +1,59 @@
 import graph.IGraph;
 
 public class Community {
-    int[][] communities;
-    int[] communitySize;
-    int[] verticeCommunity;
+    int[] community;
+    int[] sc;
     long modularity;
     IGraph graph;
 
     public Community(IGraph g) {
-        communities = new int[g.verticesCount()][1];
-        communitySize = new int[g.verticesCount()];
-        verticeCommunity = new int[g.verticesCount()];
+        community = new int[g.verticesCount()];
+        sc = new int[g.verticesCount()];
+
         graph = g;
-        
+
         for (int i = 0; i < g.verticesCount(); i++) {
-            communities[i][0] = i;
-            communitySize[i] = 1;
-            verticeCommunity[i] = i;
+            community[i] = i;
+            sc[i] = graph.degree(i);
         }
-        
+
         modularity = initModularity();
     }
 
-    public void move(int u, int from, int to) {
-        
+    public void move(int u, int to) {
+        modularity += updateModularity(u, to);
+        community[u] = to;
     }
 
     public long modularity() {
         return modularity / (2 * graph.verticesCount() * 2 * graph.verticesCount());
     }
 
-    public long updateModularity(int u, int from, int to) {
+    public long updateModularity(int u, int to) {
+        int c = community[u];
+        long duc = sumDegreeCommunity(c);
+        long dub = sumDegreeCommunity(to);
+        long du = graph.degree(u);
+        int m = graph.verticesCount();
+
+        return 4L * m * (duc + dub) - 2 * du * (sc[c] - sc[to] + du);
+    }
+
+    private long sumDegreeCommunity(int c) {
         return 0L;
     }
 
     private long initModularity() {
         long out = 0;
 
-        for (int i = 0; i < communities.length; i++) {
-            out += -graph.degree(communities[i][0]);
+        for (int i = 0; i < graph.verticesCount(); i++) {
+            out += -sc[i];
         }
 
         return out;
+    }
+
+    private void updateSc(int u) {
+
     }
 }
